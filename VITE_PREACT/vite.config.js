859299d60a40cae652ca0c010f2ba6c8e813fb35
senselogic <dev.senselogic@gmail.com>
2022-10-16 18:03:34 +0200

    % vite

--- VITE_PREACT/vite.config.js
+++ VITE_PREACT/vite.config.js
@@ -2,7 +2,6 @@ import reactor from 'senselogic-reactor-vite';
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         reactor( { framework: 'preact', include: './src/**/*.jsx' } ),
